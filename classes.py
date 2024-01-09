798888edbc13fb4c9c1ed6a954e6aa2975a7e17d
eoliennne <68473791+eoliennne@users.noreply.github.com>
2024-01-09 16:51:12 +0100

    Update classes.py
    ajout des plages ip

--- classes.py
+++ classes.py
@@ -4,8 +4,8 @@ class AS:
         self.igp = None         #IGP utilisé au sein de l'AS
         self.routers = []       #liste d'élements de type routers.
         self.rel = {}           #dico AS : relations à cet AS
-        self.ip  = None         #range ip
-        self.loopback = None    #range ip loopback
+        self.ip  = []         #range ip
+        self.loopback = []    #range ip loopback
         self.lienslocaux = {}   # dico (r1,r2) : "subnet ip"
 
 
@@ -21,7 +21,7 @@ class Router:
         self.loopback = None        #adresse de loopback du router
         self.numero = None          # numero du routeur
         self.interfaces = {}        #dico interface : [adresse ip, voisin (objet de type routeur)]
-        self.border = False         #permet de savoir si le router est en bordure de l'AS ou non
+        self.border = False         #permet de savoir si le router est en bordure de l'AS ou non 
         self.configList = []        #liste de strings correspondant aux lignes du fichier config du routeur
 
     def __repr__(self):
