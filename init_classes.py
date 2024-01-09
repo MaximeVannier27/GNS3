f72d3db67ab4e8947a019b7e690825dcff76cdba
eoliennne <68473791+eoliennne@users.noreply.github.com>
2024-01-09 16:50:36 +0100

    Update init_classes.py
    ajout addresses ipv6

--- init_classes.py
+++ init_classes.py
@@ -23,18 +23,18 @@ def init_as(dico_as):
         num_as = f"as_{as_n}"                   #nom de la variable de type AS créée
         globals()[num_as] = AS(as_n)
         globals()[num_as].igp = value["IGP"]
-        globals()[num_as].ip = value["ip_range"]
-        globals()[num_as].loopback = ipaddress.IPv6Address(value["loopback_range"])
+        globals()[num_as].ip += value["ip_range"]
+        globals()[num_as].loopback += value["loopback_range"]
         liste_as.append(globals()[num_as])      #ajout de l'AS à la liste des AS créés
 
         for (router_n,attributs) in value["routers"].items():
             Ri = f"R{router_n}" #nom de la variable de type router créée
             globals()[Ri] = Router(as_n)
             globals()[Ri].numero = router_n
-            print(globals()[Ri])
+            # print(globals()[Ri])
             (globals()[num_as].routers).append(globals()[Ri])
 
-    print(globals()[num_as])            
+    #print(globals()[num_as])            
             
     return liste_as
 
@@ -57,7 +57,11 @@ def init_routeur_adresses(dico_as):
         for router_n,connexions in value["routers"].items(): 
             Ri = f"R{router_n}"
             routeur_courant = globals()[Ri]
-            routeur_courant.loopback = loopback(as_courant.loopback,compteur_loopback)
+            loopback_temp = loopback(ipaddress.IPv6Address(as_courant.loopback[0]),compteur_loopback)
+            if loopback_temp > ipaddress.IPv6Address(as_courant.loopback[1]):
+                print("Erreur, plage d'adresses loopback débordée")
+                return -1
+            routeur_courant.loopback = loopback_temp
             compteur_loopback+=1
             
             if connexions["e_interfaces"] !=None:
@@ -68,27 +72,31 @@ def init_routeur_adresses(dico_as):
             for interface,voisin in connexions["i_interfaces"].items():
                 routeur_voisin = globals()[voisin]
                 if (routeur_voisin,routeur_courant) not in as_courant.lienslocaux.keys():
-                    as_courant.lienslocaux[(routeur_courant,routeur_voisin)] = "subnet" + str(compteur_ip)
-                    routeur_courant.interfaces[interface]=[ip_def(as_courant.ip,compteur_ip),routeur_voisin]
-                    compteur_ip+=1
-                    routeur_voisin.interfaces[interface]=[ip_def(as_courant.ip,compteur_ip),routeur_courant]
+                    subnet = ipaddress.IPv6Address(as_courant.ip[0])
+                    subnet+=compteur_ip*(2**64)
+                    if subnet > ipaddress.IPv6Address(as_courant.ip[1]):
+                        print("Erreur, plage d'adresses ip débordée")
+                        return -1
+                    as_courant.lienslocaux[(routeur_courant,routeur_voisin)] = subnet
+                    ip_1= ip_def(subnet,1)
+                    ip_2= ip_def(subnet,2)
+                    routeur_courant.interfaces[interface]=[ip_1,routeur_voisin]
+                    routeur_voisin.interfaces[interface]=[ip_2,routeur_courant]
                     compteur_ip+=1
 
-            print(f"Paramétrage du routeur R{routeur_courant.numero} :\n{routeur_courant.interfaces}\nLoopback:{routeur_courant.loopback}")        
+            #print(f"Paramétrage du routeur R{routeur_courant.numero} :\n{routeur_courant.interfaces}\nLoopback:{routeur_courant.loopback}")        
 
-"""
-ces deux fonctions sont absolument absurdes pour l'instant mdrrr c'est juste histoire de
-"""
+
 
 def loopback(range_ip,compteur):
     ip = range_ip + compteur
     return ip
 
-def ip_def(range_ip,compteur):
-    ip = str(range_ip) + str(compteur)
+def ip_def(subnet,n):
+    ip = subnet + n
     return ip
 
-#lignes de test des fonctions
-dico_json = load_intent("intent.json")
-liste_AS = init_as(dico_json)
-init_routeur_adresses(dico_json)
+# # #lignes de test des fonctions
+# dico_json = load_intent("intent.json")
+# liste_AS = init_as(dico_json)
+# init_routeur_adresses(dico_json) aha
