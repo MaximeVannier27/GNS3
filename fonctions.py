fcc0bd9c1b14ee2ffa1bb3019d806c1e4d6d0bb5
eoliennne <likavf3@gmail.com>
2024-01-17 11:24:33 +0100

    test route-map

--- fonctions.py
+++ fonctions.py
@@ -144,9 +144,25 @@ def initProtocole(routeurName,asName):
         if routeurName.border:
             for i,c in routeurName.interfaces.items():
                 if c[1].AS_n != routeurName.AS_n:
-                    lignes_protocole.append(" passive-interface {i}")
+                    lignes_protocole.append(f" passive-interface {i}")
     
     return lignes_protocole
+
+def route_map(routeurName,asName):
+    lignes_rules=[]
+    #provider
+    lignes_rules.append("route-map tomyprovider deny 20",f" match community {routeurName.numero}:300","!")
+    lignes_rules.append("route-map tomyprovider deny 21",f" match community {routeurName.numero}:100","!")
+    lignes_rules.append("route-map tomyprovider permit 30","!")
+    lignes_rules.append("route-map frommyprovider permit 20"," set local-preference 50","!")
+    #provider
+    lignes_rules.append("route-map tomyprovider deny 20",f" match community {routeurName.numero}:300","!")
+    lignes_rules.append("route-map tomyprovider deny 21",f" match community {routeurName.numero}:100","!")
+    lignes_rules.append("route-map tomyprovider permit 30","!")
+    lignes_rules.append("route-map frommyprovider permit 20"," set local-preference 50","!")
+ 
+
+
 
 
 def drag_and_drop(projet,dico,num):
