3e858b8c79ffe69c29620f79ee769f973c842c94
MaximeVannier27 <maxouvn@gmail.com>
2024-01-19 16:05:47 +0100

    Ajout de l'affichage des @ IP des routeurs

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ from init_classes import *
 
 fin_config = ["!", "!", "!", "control-plane", "!", "!", "line con 0", " exec-timeout 0 0", " privilege level 15", " logging synchronous", " stopbits 1", "line aux 0", " exec-timeout 0 0", " privilege level 15", " logging synchronous", " stopbits 1", "line vty 0 4", " login", "!", "!", "end"]
 
-dico_json = load_intent("intent_BGP_FINAL.json")
+dico_json = load_intent("intent_16-routeurs_4-AS.json")
 liste_AS = init_as(dico_json)
 init_routeur_adresses(dico_json)
 
 for AS_courant in liste_AS:
     for routeur_courant in AS_courant.routers:
-
-        print(f"Début routeur {routeur_courant.numero} de l'AS {routeur_courant.AS_n}")
-
+        
         initConfigList(routeur_courant)
         routeur_courant.configList += initInterface(routeur_courant,AS_courant)
         routeur_courant.configList += initBGP(routeur_courant,AS_courant)
@@ -25,6 +23,11 @@ for AS_courant in liste_AS:
         routeur_courant.configList += fin_config
         creationConfigFinal(routeur_courant)
         #drag_and_drop("projet_GNS3",correspondances,routeur_courant.numero)
-        print(f"Fin routeur {routeur_courant.numero} de l'AS {routeur_courant.AS_n}")
+        print(f"Routeur {routeur_courant.numero} : AS [{routeur_courant.AS_n}]")
+        print("--> ADDRESSES IP <--")
+        print(f"Interface Loopback: {routeur_courant.loopback} ")
+        for i,c in routeur_courant.interfaces.items():
+            print(f"Interface {i}: {c[0]}")
+        print("-------------------------------------------")
 
 
